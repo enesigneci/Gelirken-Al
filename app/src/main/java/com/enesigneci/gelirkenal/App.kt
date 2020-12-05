4d1bf7dac3f37f0b1915ea16ad7fb0cf1178cd4e
Enes İğneci <eigneci@magiclick.com>
2020-12-05 18:44:11 +0300

    App class added

---
+++ app/src/main/java/com/enesigneci/gelirkenal/App.kt
@@ -0,0 +1,24 @@
+package com.enesigneci.gelirkenal
+
+import android.app.Application
+import android.content.Context
+
+class App : Application() {
+
+    init {
+        instance = this
+    }
+
+    companion object {
+        private var instance: App? = null
+
+        fun applicationContext() : Context {
+            return instance!!.applicationContext
+        }
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        val context: Context = applicationContext()
+    }
+}
